4d49c62b2c1546b0274f8352bbf53fb76f0c43f5
Joana Belmiro <jnetobelmiro@gmail.com>
2021-08-19 12:09:08 +0100

    - Updated map - Updated markers - Cleaned DB

--- Chert_Source_Map/map_app.R
+++ Chert_Source_Map/map_app.R
@@ -6,6 +6,7 @@ library(leaflet.extras)
 library(tidyverse)
 library(sf)
 library(leafpop)
+library(lattice)
 #import data
 
 
@@ -35,19 +36,41 @@ ui <- fillPage(
 server <- function(input, output, session) {
   
   data <- read_csv("C:/Users/Meran/Documents/PhD/R projects/Chert_Sources_Map/Data/chert_field.csv")
-  
-  
+ 
+   getColor <- function(data) {
+    sapply(data$Name, function(Name) {
+      if(Name == "CSV") {
+        "green"
+      } else if(site == "PTA") {
+        "orange"
+      } else if(site == "PDA") {
+        "blue"
+      } else if(site == "BLX") {
+        "yellow"
+      } else if(site == "FZF") {
+        "pink"
+      } else if(site == "FR") {
+        "pink"
+      } else {
+        "red"
+      } })
+  }
   
   output$map <- renderLeaflet({
     leaflet() %>%
       #addTiles() %>% 
       addProviderTiles(provider = "Esri.WorldImagery") %>% 
       #addMarkers(lng = data$lat, lat = data$long, popup = data$NewCode) %>% 
-      addAwesomeMarkers(lng = data$long, lat = data$lat, label = data$NewCode, icon = icons, 
-                        popup = paste("Site:", data$Name, "<br>",
-                                      "Latitude:", data$lat, "<br>",
-                                      "Longitude:", data$long, "<br>",
-                                      "Description:", data$Observations))
+      #addAwesomeMarkers(lng = data$long, lat = data$lat, label = data$NewCode, icon = icons, 
+                        #popup = paste("Site:", data$Name, "<br>",
+                                     # "Latitude:", data$lat, "<br>",
+                                      #"Longitude:", data$long, "<br>",
+                                     # "Description:", data$Observations)) %>% 
+      addMarkers(clusterOptions = markerClusterOptions(), lng = data$long, lat = data$lat, label = data$NewCode, icon = icons, 
+                 popup = paste("Site:", data$Name, "<br>",
+                               "Latitude:", data$lat, "<br>",
+                               "Longitude:", data$long, "<br>",
+                               "Description:", data$Observations))
   })
     observeEvent(input$map_marker_click, { 
       p <- input$map_marker_click  # typo was on this line
